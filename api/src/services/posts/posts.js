f58fcd06430eeb972c1e638b455d933ebd7a2d10
Sean Lacy <seanl56688@gmail.com>
2025-03-10 03:24:22 -0400

    Chapter 7 Part 2

--- api/src/services/posts/posts.js
+++ api/src/services/posts/posts.js
@@ -1,18 +1,18 @@
 import { db } from 'src/lib/db'
 
 export const posts = () => {
-  return db.post.findMany()
+  return db.post.findMany({ where: { userId: context.currentUser.id } })
 }
 
 export const post = ({ id }) => {
-  return db.post.findUnique({
-    where: { id },
+  return db.post.findFirst({
+    where: { id, userId: context.currentUser.id },
   })
 }
 
 export const createPost = ({ input }) => {
   return db.post.create({
-    data: input,
+    data: { ...input, userId: context.currentUser.id },
   })
 }
 
@@ -27,4 +27,9 @@ export const deletePost = ({ id }) => {
   return db.post.delete({
     where: { id },
   })
+}
+
+export const Post = {
+  user: (_obj, { root }) =>
+    db.post.findFirst({ where: { id: root.id } }).user(),
 }
